451a29875512a1d1895ba8923cfac4ec7a8602c8
sphilippe7 <solomon924@gmail.com>
2017-01-13 11:48:01 -0500

    1st commit on Github repo
    created a repo on github to connect it to my command line

--- config/environment.js
+++ config/environment.js
@@ -3,7 +3,7 @@
 module.exports = function (environment) {
   'use strict';
   const ENV = {
-    modulePrefix: 'ga-wdi-boston.ember-auth',
+    modulePrefix: 'rate-my-photos',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
---
+++ tests/unit/your-photos/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:your-photos', 'Unit | Route | your photos', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
